ee63f8cc472ced20312f0fb3adf3e94b370ffa46
Dmytro Osaulenko <osauldmy@gmail.com>
2024-12-01 17:34:51 +0100

    test: add test for 1st day and mention tests running in readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 My solutions to Advent of Code 2024
+
+
+Run locally with installed Elixir: `mix test --trace`
---
+++ test/aoc2024_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2024Test do
+  use ExUnit.Case
+
+  test "day1" do
+    assert Day01.first() == 2_378_066
+    assert Day01.second() == 18_934_359
+  end
+end
