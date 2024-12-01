8e935dc68d375a1d981d44831f2a7b75eaa99c55
Dmytro Osaulenko <osauldmy@gmail.com>
2024-12-01 17:33:04 +0100

    chore: add mix project files + README

---
+++ mix.exs
@@ -0,0 +1,12 @@
+defmodule Aoc2024.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :aoc_2024,
+      version: "0.1.0",
+      elixir: "~> 1.17",
+      deps: []
+    ]
+  end
+end
